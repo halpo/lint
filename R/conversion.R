411885b4f6b88a5a0ee29b09b97200d99be6a5c4
Andrew Redd <amredd@gmail.com>
2012-06-16 07:07:19 -0600

    separated family and conversion functions out of R/lint.R

---
+++ R/conversion.R
@@ -0,0 +1,203 @@
+{############################################################################### 
+# Conversion.R
+# This file is part of the R package lint.
+# 
+# Copyright 2012 Andrew Redd
+# Date: 6/16/2012
+# 
+# DESCRIPTION
+# ===========
+# functions for conversion between the difference formats
+# 
+# LICENSE
+# ========
+# harvestr is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software 
+# Foundation, either version 3 of the License, or (at your option) any later 
+# version.
+# 
+# dostats is distributed in the hope that it will be useful, but WITHOUT ANY 
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# dostats. If not, see http://www.gnu.org/licenses/.
+# 
+}###############################################################################
+
+#' @name conversion
+#' @title Lint internal data structures
+#' @keywords utils, internal
+#' 
+#' @section Introduction
+#'  lint makes use of several functions from different packages that 
+#'  store data in various different formats.  These functions provide 
+#'  utilities for converting between the different formats.
+#'  
+#'  The formats are:
+#'  \enumerate{
+#'      \item parse   - data from the data atribute of a \code{\link{parser}}
+#'                      object.  In parse data each element of an expression
+#'                      has it's own row.
+#'      \item find    - similar to parse but gives a row for each region or 
+#'                      expression of interest.
+#'      \item replace - for use with \code{\link[str_sub]{stringr}}.  
+#'                      Uses a column structure with start and end, organized
+#'                      into a matrix with a row for each line.
+#'      \item locate  - results from \code{\link{str_locate}} from stringr.
+#'                      same as replace for most purposes but does not include
+#'                      a string.
+#'  }
+#'  
+#'  
+#'  Converts data from the results of \code{\link{parser}}
+#'
+#'  @section find data structure
+#'   For the purposes of the data the find data consists of a single row 
+#'   for each section/region that 
+#'   contains the columns \code{line1}, \code{col1}, \code{byte1}, \code{line2}, 
+#'   \code{col2}, and \code{byte2}, marking the beginning and end of a section.
+#'   This is a condensation of the parse data which would have the same columns
+#'   as well as additional columns, and a row for each expression in the region.
+#'  
+#'
+#'  @section Replace data structure
+#'   The data structure for replace data is defined as a data frame with 
+#'   columns suitable for use ase arguments to str_sub.  That is it has columns
+#'   \enumerate{
+#'     \item \code{start}
+#'     \item \code{end}
+#'     \item and either \code{string} or \code{line}
+#'   }
+#'   where \code{string} would be preferred but line to match up with line data.
+#'   \code{find2replace} uses the line, since the string is not available in the
+#'   \link[parse2find]{find data}.
+#'
+#' @section Locate data structure
+#'  locate data is defined as the matrix that comes from \code{\link{str_locate}}.
+#'  It has columns
+#'  \enumerate{
+#'      \item \code{start}
+#'      \item \end{end}
+#'  }
+#'  and has a row for every line.
+empty.find <- {data.frame(
+      'line1' = integer(0L)
+    , 'col1'  = integer(0L)
+    , 'byte1' = integer(0L)
+    , 'line2' = integer(0L)
+    , 'col2'  = integer(0L)
+    , 'byte2' = integer(0L))}
+
+#' @rdname conversion
+#' @export
+parse2find <- function(parse.data) {
+  if (!inherits(parse.data, 'data.frame') && inherits(parse.data, 'list')) {
+    return(ldply(parse.data, parse2find))
+  }
+  names1 <- c('line1', 'col1', 'byte1')
+  names2 <- c('line2', 'col2', 'byte2')
+  return(data.frame(
+      parse.data[1L, names1]
+    , tail(parse.data, 1L)[1L, names2]
+  ))
+}
+
+#' @rdname conversion
+#' @export
+find2replace <- function(find.data) {
+  mdply(find.data, function(line1, byte1, line2, byte2, ...){
+    if (line1==line2) {
+      data.frame(line=line1, start = byte1 + 1, end = byte2)
+    } else {
+      nlines = line2-line1
+      data.frame(
+        line  = c(line1:line2), 
+        start = c(byte1+1, rep(1L, nlines)),
+        end   = c(rep(-1L, nlines), byte2))
+    }
+  })[, -seq_len(ncol(find.data))]
+}
+
+#' @rdname conversion
+#' @export
+locate2find <- function(loc) {
+    if(all(is.na(loc$start)))return(empty.find)
+    if(!inherits(loc, .data.frame))
+        loc <- as.data.frame(loc)
+    mutate(loc
+      , line1 = seq_along(start)
+      , line2 = seq_along(start)
+      ,  col1 = start
+      , byte1 = start
+      ,  col2 = end
+      , byte2 = end)[!is.na(loc$start), names(empty.find)]
+}
+
+
+do_results_overlap <- function(x, y=x) {
+  if (nrow(x) > 1) {
+    x   <- mlply(x,data.frame)
+    res <- llply(x, do_results_overlap, y=y)
+    return(laply(res, noattr))
+  }
+  if (nrow(y) > 1) {
+    y <- mlply(y,data.frame)
+    res <- llply(y, do_results_overlap, x=x)
+    return(laply(res, noattr))
+  }
+  if (x$line2 < y$line1) return(FALSE)
+  if (x$line1 > y$line2) return(FALSE)
+  x.start <- x$line1
+  x.end   <- x$line2
+  y.start <- y$line1
+  y.end   <- y$line2
+  max.byte <- max(x$byte1, x$byte2, y$byte1, y$byte2)
+  if (max.byte>0) {
+    x.start <- x.start + x$byte1/max.byte
+    x.end   <- x.end   + x$byte2/max.byte 
+    y.start <- y.start + y$byte1/max.byte
+    y.end   <- y.end   + y$byte2/max.byte
+  }
+  if (x.start < y.start && y.start < x.end) return(TRUE)
+  if (x.start < y.end   && y.end   < x.end) return(TRUE)
+  if (y.start < x.start && x.start < y.end) return(TRUE)
+  if (y.start < x.end   && x.end   < y.end) return(TRUE)
+  return(FALSE)
+}
+
+merge_find <- function(...){
+  # merge multiple find results
+  # @param ... find results.
+  find.results <- c(list(), ...)
+  if (F) {
+    parse.data=stop()
+    x <- find_function_args(parse.data=parse.data)
+    y <- find_call_args(parse.data=parse.data)
+    x <- x[1,]
+    y <- y[1,]
+    
+    x.idx <- overlaps[1, 1]
+    y.idx <- overlaps[1, 2]
+  }
+  overlaps <- data.frame(which(do_results_overlap(x,y), arr.ind=T))
+  names(overlaps) <- c('x.idx', 'y.idx')
+  merged <- mdply(overlaps, function(x.idx, y.idx, x, y){
+    x.row <- x[x.idx, ]
+    y.row <- y[y.idx, ]
+    data.frame(
+      line1 = min(x.row$line1, y.row$line1),
+       col1 = min( x.row$col1,  y.row$col1),
+      byte1 = min(x.row$byte1, y.row$byte1),
+      line2 = min(x.row$line2, y.row$line2),
+       col2 = min( x.row$col2,  y.row$col2),
+      byte2 = min(x.row$byte2, y.row$byte2))
+  }, x=x, y=y)
+  keep <- c('line1', 'col1', 'byte1', 'line2', 'col2', 'byte2')
+  new.finds <- rbind(merged[keep],
+    x[-overlaps$x.idx, keep],
+    y[-overlaps$y.idx, keep])
+  #' @results a single \code{\link{data.frame}} with find results where overlaps
+  #'  were merged
+  new.finds[do.call(order, new.finds), ]
+}
--- R/lint.R
+++ R/lint.R
@@ -11,7 +11,10 @@
 #' @importFrom parser parser
 #' @importFrom harvestr noattr
 #' @importFrom dostats collect
+#' @import dostats
 #' @include lint.patterns.R
+#' @include conversion.R
+#' @include family.R
 NULL
 
 { # TODO
@@ -78,42 +81,18 @@ dispatch_test <- function(test, file, parse.data=attr(parser(file), 'data')
 ) {
   include.region <- with_default(test$include.region, character(0))
   if (length(include.region)>=1L) {
+    stop("include.region not yet supported.")
+    fun.in.region <- find_fun(include.region)
+    in.fe <- foreach(fun=fun.in.region, .combine=rbind.fill, .multicombine=TRUE)
+    in.regions <- in.fe %do% fun(file=file, lines=lines, parse.data=parse.data)
   }
   
-  exclude.region <- with_default(test$exclude.region, c("comment", "string"))
-  
+  exclude.region <- with_default(test$exclude.region, c("find_comment", "find_string"))
   if (length(exclude.region)> 0L) {
-    char.ex.region.idx <- laply(exclude.region, inherits, 'character')
-    fun.ex.region.idx  <- laply(exclude.region, inherits, 'function')
-    if (!all(char.ex.region.idx | fun.ex.region.idx)) {
-      stop("Exclude regions must be either character strings or functions that return find data.")
-    }
-    fun.ex.region <- as.list(exclude.region )
-    ex.region.names  <- paste("find", exclude.region[char.ex.region.idx], sep='_')
-    mf <- function(...)match.fun(...)
-    for(i in which(char.ex.region.idx)) {
-        fun.ex.region[[i]] <- try(get(ex.region.names[i], mode = 'function'
-                                      , inherits=T), silent=TRUE)
-        if(inherits(fun.ex.region[[i]], 'try-error'))
-            fun.ex.region[[i]] <- match.fun(ex.region.names[i])
-    }
-    for(fun.ex in fun.ex.region){
-      lines <- fun.ex(lines=lines)
-    }
-    
-    # find method
-    find.region.names  <- paste("find", exclude.region[char.ex.region.idx], sep='_')
-    fun.find.region <- as.list(exclude.region)
-    for(i in which(char.ex.region.idx)) {
-        fun.find.region[[i]] <- try(get(ex.region.names[i], mode = 'function'
-                                      , inherits=T), silent=TRUE)
-        if(inherits(fun.find.region[[i]], 'try-error'))
-            fun.find.region[[i]] <- match.fun(ex.region.names[i])
-    }
-    ex.regions <- 
-        llply(fun.find.region, do.call, list(parse.data=parse.data))
-    
-  } 
+    fun.ex.region <- find_fun(exclude.region)
+    ex.fe <- foreach(fun=fun.ex.region, .combine=rbind.fill, .multicombine=TRUE)
+    ex.regions <- ex.fe %do% fun(file=file, lines=lines, parse.data=parse.data)
+  }
   
   use.lines = with_default(test$use.lines, TRUE)
   if (!use.lines) lines <- paste(lines, '\n', collapse='')
@@ -150,8 +129,9 @@ dispatch_test <- function(test, file, parse.data=attr(parser(file), 'data')
 #' @return returns an integer vector of the problem lines if problems were 
 #'   found. otherwise returns TRUE if the lines pass the check. 
 #'   \code{\link{isTRUE}} could be useful for checking the return value.
-#' @export
+#' @return \link[parse2find]{find} compatible format.
 #' @family lint
+#' @export
 check_pattern <- function(pattern
   , lines
   , ...) {
@@ -162,7 +142,7 @@ if(F){
     problem.yn <- llply(pattern, grepl, lines, perl=T)
     problem.yn <- collect(problem.yn, `|`)
   } else {
-    problem.yn    <- grepl(pattern, lines, perl=T)
+    problem    <- str_locate(perl(pattern), lines)
   }
   problem.lines <- which(problem.yn)
   if (any(problem.lines)) {
@@ -172,188 +152,16 @@ if(F){
   }
 }
 #} # Core Functions
-{ # Conversion
-parse2find <- function(parse.data) {
-#'  Convert parser Structured data to find structured data
-#'  
-#'  Converts data from the results of \code{\link{parser}}
-#'  
-#'  @section find data structure 
-#'  For the purposes of the data the find data consists of a single row 
-#'  for each section/region that 
-#'  contains the columns \code{line1}, \code{col1}, \code{byte1}, \code{line2}, 
-#'  \code{col2}, and \code{byte2}, marking the beginning and end of a section.
-#'  This is a condensation of the parse data which would have the same columns
-#'  as well as additional columns, and a row for each expression in the region.
-#'  
-#'  @export
-#'  @keywords utils
-#'  @family type-convert
-#'  @family find-functions
-  if (!inherits(parse.data, 'data.frame') && inherits(parse.data, 'list')) {
-    return(ldply(parse.data, parse2find))
-  }
-  names1 <- c('line1', 'col1', 'byte1')
-  names2 <- c('line2', 'col2', 'byte2')
-  return(data.frame(
-      parse.data[1L, names1]
-    , tail(parse.data, 1L)[1L, names2]
-  ))
-}
-find2replace <- function(find.data) {
-#' Convert find structured data to replace structured data
-#'
-#' Converts find data to data that is suitable for use with replace function.
-#'
-#'  @section Replace data structure
-#'  The data structure for replace data is defined as a data frame with 
-#'  columns suitable for use ase arguments to str_sub.  That is it has columns
-#'  \enumerate{
-#'    \item \code{start}
-#'    \item \code{end}
-#'    \item and either \code{string} or \code{line}
-#'  }
-#'  where \code{string} would be preferred but line to match up with line data.
-#'  \code{find2replace} uses the line, since the string is not available in the
-#'  \link[parse2find]{find data}.
-#'
-#'
-#' @param find.data find structured data
-#' @export
-#' @keywords utils
-#' @family type-convert
-#'  @family find-functions
-  mdply(find.data, function(line1, byte1, line2, byte2, ...){
-    if (line1==line2) {
-      data.frame(line=line1, start = byte1 + 1, end = byte2)
-    } else {
-      nlines = line2-line1
-      data.frame(
-        line  = c(line1:line2), 
-        start = c(byte1+1, rep(1L, nlines)),
-        end   = c(rep(-1L, nlines), byte2))
-    }
-  })[, -seq_len(ncol(find.data))]
-}
-do_results_overlap <- function(x, y=x) {
-  if (nrow(x) > 1) {
-    x   <- mlply(x,data.frame)
-    res <- llply(x, do_results_overlap, y=y)
-    return(laply(res, noattr))
-  }
-  if (nrow(y) > 1) {
-    y <- mlply(y,data.frame)
-    res <- llply(y, do_results_overlap, x=x)
-    return(laply(res, noattr))
-  }
-  if (x$line2 < y$line1) return(FALSE)
-  if (x$line1 > y$line2) return(FALSE)
-  x.start <- x$line1
-  x.end   <- x$line2
-  y.start <- y$line1
-  y.end   <- y$line2
-  max.byte <- max(x$byte1, x$byte2, y$byte1, y$byte2)
-  if (max.byte>0) {
-    x.start <- x.start + x$byte1/max.byte
-    x.end   <- x.end   + x$byte2/max.byte 
-    y.start <- y.start + y$byte1/max.byte
-    y.end   <- y.end   + y$byte2/max.byte
-  }
-  if (x.start < y.start && y.start < x.end) return(TRUE)
-  if (x.start < y.end   && y.end   < x.end) return(TRUE)
-  if (y.start < x.start && x.start < y.end) return(TRUE)
-  if (y.start < x.end   && x.end   < y.end) return(TRUE)
-  return(FALSE)
-}
-merge_find <- function(...){
-  # merge multiple find results
-  # @param ... find results.
-  find.results <- c(list(), ...)
-  if (F) {
-    parse.data=stop()
-    x <- find_function_args(parse.data=parse.data)
-    y <- find_call_args(parse.data=parse.data)
-    x <- x[1,]
-    y <- y[1,]
-    
-    x.idx <- overlaps[1, 1]
-    y.idx <- overlaps[1, 2]
-  }
-  overlaps <- data.frame(which(do_results_overlap(x,y), arr.ind=T))
-  names(overlaps) <- c('x.idx', 'y.idx')
-  merged <- mdply(overlaps, function(x.idx, y.idx, x, y){
-    x.row <- x[x.idx, ]
-    y.row <- y[y.idx, ]
-    data.frame(
-      line1 = min(x.row$line1, y.row$line1),
-       col1 = min( x.row$col1,  y.row$col1),
-      byte1 = min(x.row$byte1, y.row$byte1),
-      line2 = min(x.row$line2, y.row$line2),
-       col2 = min( x.row$col2,  y.row$col2),
-      byte2 = min(x.row$byte2, y.row$byte2))
-  }, x=x, y=y)
-  keep <- c('line1', 'col1', 'byte1', 'line2', 'col2', 'byte2')
-  new.finds <- rbind(merged[keep],
-    x[-overlaps$x.idx, keep],
-    y[-overlaps$y.idx, keep])
-  #' @results a single \code{\link{data.frame}} with find results where overlaps
-  #'  were merged
-  new.finds[do.call(order, new.finds), ]
-}
-} # Conversion
-{ # Family Functions
-get_child <- function(id, parse.data, nlevels=-1L, include.parent=TRUE) {
-#'  @rdname get_children
-#'  @export
-  stopifnot(length(id)==1)
-  ids <- id
-  while(nlevels!=0) {
-    nlevels <- nlevels - 1
-    old.ids <- ids
-    parse.sub <- subset(parse.data, parse.data$parent %in% ids)
-    if(include.parent) ids <- c(id, parse.sub$id) else ids <- parse.sub$id       
-    if (identical(ids, old.ids)) break 
-  }
-  parse.sub
-}
-get_children <- function(id, parse.data, nlevels=-1L){
-#' Find the children of an expression
-#' 
-#'  This takes the \code{parse.data} and find all the children of the expression
-#'  with the given \code{id}.
-#' 
-#'  @param id the id of the given expression in \code{parse.data}
-#'  @param parse.data the data from a parsed file or expression.  
-#'    The results of \code{\link{parser}}.
-#'  @param nlevels the number of levels to search.  If a negative number is 
-#'    given all children will be found.
-#' 
-#' @family  parse-functions
-#' @export
-  if (inherits(id, 'data.frame') && 'id' %in% names(id))
-    id <- id$id
-  stopifnot(inherits(id, 'integer'))
-  return(alply(id, 1, get_child, parse.data=parse.data, nlevels=nlevels))
-}
-find_children <- function(...){
-#'  @rdname get_children
-#'  @export
-  parse2find(get_children(...))
-}
-get_parent <- function(id, parse.data) {
-  parse.data[parse.data$id == id, 'parent']
-}
-get_family <- function(id, parse.data, nancestors=0L, nchildren=Inf){
-  parents <- id
-  while(nancestors > 0L){
-    nancestors <- nancestors -1
-    nchildren <- nchildren + 1
-    parents <- get_parent(parents, parse.data)
-  }
-  get_child(parents, parse.data, nchildren)
-}
-} # Family Functions
 { # strip/extract utilities
+find_fun <- Vectorize(function(ex){
+    if(is.function(ex)) return(ex)
+    stopifnot(is.character(ex))
+    fun <- try(get(ex, mode = 'function', inherits=T), silent=TRUE)
+    if(!inherits(fun, 'try-error')) return(fun)
+    fun <- try(match.fun(ex.region.names[i]))
+    if(!inherits(fun, 'try-error')) return(fun)
+    stop(sprintf("Could not find `%s`", ex))
+}, SIMPLIFY=TRUE)
 strip <- function(lines, replace.data, replace.with=''){
 #'  Strip a region from the text
 #'  
@@ -416,26 +224,23 @@ make_extractor <- function(finder){
 }
 } # strip/extract utilities
 { # Region Finders
-{ # comment
-find_comment <- function(parse.data) {
-  comment.data <- subset(parse.data
-    , parse.data$token.desc %in% c("COMMENT", "ROXYGEN_COMMENT")
-  )
+make_class_finder <- function(classes){
+    structure(function(parse.data, ...) {
+        subset(parse.data, parse.data$token.desc %in% classes)
+    }, classes=classes)
 }
+{ # comment
+find_comment <- make_class_finder(c("COMMENT", "ROXYGEN_COMMENT"))
 strip_comment   <- make_stripper(find_comment)
 extract_comment <- make_extractor(find_comment)
 } # comment
 { # string
-find_string <- function(parse.data){
-  subset(parse.data,
-    parse.data$token.desc %in% c("STR_CONST")
-  )
-}
+find_string <- make_class_finder(c("STR_CONST"))
 strip_string   <- make_stripper(find_string, replace.with='""')
 extract_string <- make_extractor(find_string)
 } # string
 { # function
-find_function_args <- function(parse.data) {
+find_function_args <- function(parse.data, ...) {
   ftokens <- subset(parse.data, parse.data$token.desc=="FUNCTION")
   ddply(ftokens, "id" , function(d, ..., parse.data) {
     p <- d$parent
@@ -446,7 +251,7 @@ find_function_args <- function(parse.data) {
 }
 strip_function_args   <- make_stripper(find_function_args, replace.with="()")
 extract_function_args <- make_extractor(find_function_args)
-find_function_body <- function(file, parse.data = attr(parser(file))) {
+find_function_body <- function(file, parse.data = attr(parser(file)), ...) {
   f.nodes <- subset(parse.data, parse.data$token.desc == "FUNCTION")
   body.parents  <- ldply(get_children(f.nodes$parent, parse.data, 1), tail, 1)
   body.contents <- find_children(body.parents, parse.data)
@@ -456,12 +261,12 @@ strip_function_body <- make_stripper(find_function_body, replace.with="{}")
 extract_function_body <- make_extractor(find_function_body)
 } # function
 { # call args
-get_call_args <- function(file, parse.data=attr(parser(file))){
+get_call_args <- function(file, parse.data=attr(parser(file)), ...) {
   call.nodes <- subset(parse.data, 
     parse.data$token.desc == "SYMBOL_FUNCTION_CALL")
   llply(call.nodes$id, get_family, parse.data=parse.data, nancestors=2)
 }
-find_call_args <- function(file, parse.data=attr(parser(file))){
+find_call_args <- function(file, parse.data=attr(parser(file)), ...) {
   parse2find(get_call_args(parse.data=parse.data))
 }
 strip_call_args   <- make_stripper(find_call_args, replace.with="")
