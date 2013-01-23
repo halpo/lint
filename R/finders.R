1f1e45700c8a7d523baa9183973167a24d1ea624
Andrew Redd <amredd@gmail.com>
2013-01-23 12:08:50 -0700

    Bug fix for check warning.

--- R/finders.R
+++ R/finders.R
@@ -78,9 +78,9 @@ find_basic_comment <- make_class_finder("COMMENT")
 #' @rdname finders
 #' @export
 find_inside_comment <- function(...,parse.data) {
-    df <- mutate(find_basic_comment(..., parse.data = parse.data)
-                , col1 = col1 + 1)
-    subset(df, col2 > col1 | line2 > line1)
+    df <- find_basic_comment(..., parse.data = parse.data)
+    df$col1 <- df$col1 + 1
+    subset(df, df$col2 > df$col1 | df$line2 > df$line1)
 }
 
 #' @rdname finders
